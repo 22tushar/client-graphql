d3ca92091b2ff851031e8b55215f8bdfda837244
22tushar <gehlottushar18@gmail.com>
2023-01-28 23:27:49 +0530

    add fields

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+export default function App() {
 
-function App() {
+  const [Movie,setMovie]=useState('');
+ 
+  const handleChange = (e) => {
+       setMovie(e.target.value)
+       console.log(Movie)
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setMovie('');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    <div>
+      <div className='App' >Add Movie</div>
+
+      <div className='container'>
+      <form onSubmit={handleSubmit}>
+      <label>
+        <input type="text" placeholder='Enter Movie Name' value={Movie} onChange={handleChange} />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+      </div>
+    
+    </div>
+
+  )
+}
