2c81ade62c5bafe05e8187220fef3f1e672c7bac
22tushar <gehlottushar18@gmail.com>
2023-01-29 21:58:02 +0530

    add getMovies query

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React, { useState } from 'react'
 import './App.css'
+import { useQuery, gql } from '@apollo/client';
 export default function App() {
 
-  const [Movie,setMovie]=useState('');
- 
-  const handleChange = (e) => {
-       setMovie(e.target.value)
-       console.log(Movie)
+  const GET_ALL_MOVIES = gql`  
+query {
+  getMovies{
+      name
+      rating
   }
+}
+`
+  // const [Movie,setMovie]=useState('');
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    setMovie('');
-  }
+  // const handleChange = (e) => {
+  //      setMovie(e.target.value)
+  //      console.log(Movie)
+  // }
+
+  // const handleSubmit = event => {
+  //   event.preventDefault();
+  //   setMovie('');
+  // }
+
+  const { loading, error, data } = useQuery(GET_ALL_MOVIES);
+
+  if (loading) return <div>loading...</div>
+
+  if (error) console.log(error.message)
   return (
 
-    <div>
-      <div className='App' >Add Movie</div>
-
+    <div className='App' >
       <div className='container'>
+        <table>
+          
+        {data.getMovies.map((value,index) => {
+          return (
+            <>
+               <tr key={index}>
+              <td>{value.name}</td>
+              <td>{value.rating}</td>
+            </tr>
+            </>
+          )
+        })}
+        </table>
+      </div>
+    </div>
+  )
+}
+
+{/* <div className='container'>
       <form onSubmit={handleSubmit}>
       <label>
         <input type="text" placeholder='Enter Movie Name' value={Movie} onChange={handleChange} />
@@ -26,8 +58,4 @@ export default function App() {
       <button type="submit">Submit</button>
     </form>
       </div>
-    
-    </div>
-
-  )
-}
+     */}
